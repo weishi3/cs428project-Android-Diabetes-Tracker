7ad6ef0c833a68c004801e9eaa03104055f39044
Qi-Z <zhang.qi.js@gmail.com>
2016-02-28 00:43:41 -0600

    1. Move logout to overflow menu 2. Make diagnosis a card qizhang4 & yjiang32

--- DiabetesTracker/app/src/main/java/com/cs428/dit/diabetestracker/MainActivity.java
+++ DiabetesTracker/app/src/main/java/com/cs428/dit/diabetestracker/MainActivity.java
@@ -3,9 +3,12 @@ package com.cs428.dit.diabetestracker;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -22,21 +25,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         session = new SessionManager(getApplicationContext());
-        Button logoutBtn = (Button) findViewById(R.id.logoutBtn);
-        Button diagnosisBtn = (Button) findViewById(R.id.diagnosisBtn);
-
         ImageView profileAvatar = (ImageView) findViewById(R.id.profileAvatar);
-
-        logoutBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Clear the session data
-                // This will clear all session data and
-                // redirect user to LoginActivity
-                session.logoutUser();
-                finish();
-            }
-        });
+        CardView diagnosisCard = (CardView) findViewById(R.id.diagnosis_card_view);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
 
         //Go to profile page when the user click the avatar
         profileAvatar.setOnClickListener(new View.OnClickListener() {
@@ -47,19 +39,42 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //Go to diagnosis page when the user click the button
-        diagnosisBtn.setOnClickListener(new View.OnClickListener() {
+        //Go to diagnosis page when the user click the card
+        diagnosisCard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent diagnosis = new Intent(getApplicationContext(), DiagnosisActivity.class);
                 startActivity(diagnosis);
             }
         });
+
         TextView welcomeTxt = (TextView) findViewById(R.id.welcomeTxt);
         welcomeTxt.setText(session.getUserDetails().get(SessionManager.KEY_EMAIL));
         Log.d(TAG, "user is " + welcomeTxt.getText().toString());
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //handle logout
+        if (id == R.id.action_logout) {
+            session.logoutUser();
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
