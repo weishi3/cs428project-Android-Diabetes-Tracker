673a0a7fc37e193b0be68edf34e303b9f82e1521
weishi3 <weishi3@illinois.edu>
2016-04-07 05:15:09 -0500

    monitor setting

--- DiabetesTracker/app/src/main/java/com/cs428/dit/diabetestracker/helpers/monitorSetting.java
+++ DiabetesTracker/app/src/main/java/com/cs428/dit/diabetestracker/helpers/monitorSetting.java
@@ -5,20 +5,20 @@ import java.util.ArrayList;
 /**
  * Created by water on 4/6/16.
  */
-public class monitorSetting {
+public class MonitorSetting {
 
     public int numDaysMonitor;
     public String indicatorType;
     public ArrayList<String> warningMessage;
 
-    public monitorSetting(){
+    public MonitorSetting(){
         numDaysMonitor = 3;
         indicatorType = "bloodSugar";
         warningMessage = new ArrayList<>();
         warningMessage.add("bloodSugar");
     }
 
-    public monitorSetting(int days, String indicatorType, ArrayList<String> warningMessage){
+    public MonitorSetting(int days, String indicatorType, ArrayList<String> warningMessage){
         numDaysMonitor = days;
         this.indicatorType = indicatorType;
         this.warningMessage =  warningMessage;
