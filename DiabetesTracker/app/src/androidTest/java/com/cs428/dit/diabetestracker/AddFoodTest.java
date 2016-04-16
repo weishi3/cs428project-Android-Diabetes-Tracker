cde0e040a8676abbe17b4d977f8fe91b050d8340
Qi Zhang <zhang.qi.js@gmail.com>
2016-04-16 15:35:10 -0500

    qizhang4&yjiang32

--- DiabetesTracker/app/src/androidTest/java/com/cs428/dit/diabetestracker/AddFoodTest.java
+++ DiabetesTracker/app/src/androidTest/java/com/cs428/dit/diabetestracker/AddFoodTest.java
@@ -1,11 +1,16 @@
 package com.cs428.dit.diabetestracker;
 
 import android.support.test.rule.ActivityTestRule;
+import android.util.Log;
 
+import com.cs428.dit.diabetestracker.helpers.SessionManager;
 import com.firebase.client.Firebase;
 
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
+
+import java.util.Date;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
@@ -16,6 +21,7 @@ import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
+
 /**
  * Created by QiZhang on 3/16/16.
  * Test with account test1@test.com
@@ -24,38 +30,59 @@ public class AddFoodTest {
     @Rule
     public ActivityTestRule<AddFoodItemActivity> addFoodItemActivityActivityTestRule =
             new ActivityTestRule<>(AddFoodItemActivity.class);
-    String fbURL = "https://brilliant-fire-9755.firebaseio.com/foodStats/test1@test!com";
+
+    private SessionManager sessionManager;
+    String fbURL;
+    @BeforeClass
+    public void initialize(){
+        sessionManager = new SessionManager(addFoodItemActivityActivityTestRule.getActivity().getBaseContext());
+        fbURL = "https://brilliant-fire-9755.firebaseio.com/foodStats/"+(sessionManager.getUserDetails().get(SessionManager.KEY_EMAIL)+"").replace('.','!');
+    }
+//    String fbUrl
+//    String fbURL = "https://brilliant-fire-9755.firebaseio.com/foodStats/test1@test!com";
 
     @Test
     public void addFoodItemSuccessfulTest1(){
+        Log.d("CURRENT_USER: ", sessionManager.getUserDetails().get(SessionManager.KEY_EMAIL)+"");
+        Log.d("EMAIL",fbURL);
+        Date date = new Date();
         Firebase ref = new Firebase(fbURL);
         onView(withId(R.id.foodNameTextfield))
-                .perform(typeText("test_food"), closeSoftKeyboard());
+                .perform(typeText("test_food"+date), closeSoftKeyboard());
         onView(withId(R.id.foodCaloriesTextfield))
                 .perform(typeText("250"), closeSoftKeyboard());
         onView(withId(R.id.sugarInGramsTextfield))
                 .perform(typeText("5"), closeSoftKeyboard());
         onView(withId(R.id.btn_save_food_item))
                 .perform(click());
-        onView(withText("test_food"))
+        try {
+            Thread.sleep(2500);
+        } catch (InterruptedException e) {
+        }
+        onView(withText("test_food"+date))
                 .check(matches(isDisplayed()));
-        ref.removeValue();
+
     }
 
     @Test
     public void addFoodItemSuccessfulTest2(){
         Firebase ref = new Firebase(fbURL);
+        Date date = new Date();
         onView(withId(R.id.foodNameTextfield))
-                .perform(typeText("test_food2"), closeSoftKeyboard());
+                .perform(typeText("test_food2"+date), closeSoftKeyboard());
         onView(withId(R.id.foodCaloriesTextfield))
                 .perform(typeText("250"), closeSoftKeyboard());
         onView(withId(R.id.sugarInGramsTextfield))
                 .perform(typeText("5"), closeSoftKeyboard());
         onView(withId(R.id.btn_save_food_item))
                 .perform(click());
-        onView(withText("test_food2"))
+        try {
+            Thread.sleep(2500);
+        } catch (InterruptedException e) {
+        }
+        onView(withText("test_food2"+date))
                 .check(matches(isDisplayed()));
-        ref.removeValue();
+        // ref.removeValue();
     }
 
 
