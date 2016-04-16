bf6a46ae18bef2dff3d0704712eee7a3dfe723b8
Qi Zhang <zhang.qi.js@gmail.com>
2016-04-16 14:15:11 -0500

    Change tutorial style. qizhang4&yjiang32

--- DiabetesTracker/app/src/main/java/com/cs428/dit/diabetestracker/helpers/ShowcaseManager.java
+++ DiabetesTracker/app/src/main/java/com/cs428/dit/diabetestracker/helpers/ShowcaseManager.java
@@ -3,7 +3,10 @@ package com.cs428.dit.diabetestracker.helpers;
 import android.app.Activity;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
+import com.cs428.dit.diabetestracker.R;
 import com.github.amlcurran.showcaseview.OnShowcaseEventListener;
 import com.github.amlcurran.showcaseview.ShowcaseView;
 import com.github.amlcurran.showcaseview.targets.ViewTarget;
@@ -67,11 +70,23 @@ public class ShowcaseManager {
      */
     public void showShowCaseView(ShowcaseParam showcaseParam) {
         requestFocus(showcaseParam.getMainActivity(), showcaseParam.getId());
+
+        RelativeLayout.LayoutParams lps = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+// This aligns button to the bottom left side of screen
+        lps.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+        lps.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+// Set margins to the button, we add 16dp margins here
+        int margin = ((Number) (showcaseParam.getMainActivity().getResources().getDisplayMetrics().density * 16)).intValue();
+        lps.setMargins(margin, margin, margin, margin);
+
+
         showcaseParam.setShowcaseView(new ShowcaseView.Builder(showcaseParam.getMainActivity())
                 .withMaterialShowcase()
+                .setStyle(R.style.ShowcaseViewStyle)
                 .setTarget(new ViewTarget(showcaseParam.getId(), showcaseParam.getMainActivity()))
                 .setContentTitle(showcaseParam.getContentTitle())
                 .setContentText(showcaseParam.getContentText())
+                .blockAllTouches()
                 .singleShot(showcaseParam.getShotId())
                 .setShowcaseEventListener(new OnShowcaseEventListener() {
                     @Override
@@ -95,7 +110,12 @@ public class ShowcaseManager {
 
                     }
                 }));
-        showcaseParam.getShowcaseView().build();
+        ShowcaseView sv = showcaseParam.getShowcaseView().build();
+
+
+        sv.setButtonPosition(lps);
+
+
 
     }
 }
