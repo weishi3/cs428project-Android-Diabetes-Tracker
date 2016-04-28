9e46f76dd01abd63375d8e98f03831a5f49eaf5d
weishi3 <weishi3@illinois.edu>
2016-04-27 21:51:05 -0500

    more adv test cases

--- DiabetesTracker/app/src/androidTest/java/com/cs428/dit/diabetestracker/DiagnosisInfoTest.java
+++ DiabetesTracker/app/src/androidTest/java/com/cs428/dit/diabetestracker/DiagnosisInfoTest.java
@@ -49,6 +49,42 @@ public class DiagnosisInfoTest {
     }
 
 
+    @Test
+    public void testCheater2() {
+
+        User u = new User(21.0f, 72.0f, 25, 123, true, false);
+        u.setSedentaryJob(true);
+        u.setExerciseT(70);
+
+        u.setDiagnosedD(false);
+        u.setGDM(true);
+        u.setWeightB(5);
+        u.setHDL_C(0.88);
+        u.setTG(2.3);
+
+        List<String> sug = u.generateSuggestion();
+        assertEquals(u.isElder(), false);
+        assertTrue(!sug.contains("Your lifestyle can be classified as sedentary, do more exercise!"));
+        assertTrue(!sug.contains("Since you are elder than 40, you've got a higher risk of diabetes and cardiovascular disease. Take physical examination more often!"));
+        assertTrue(sug.contains("Giant infant symptom means you have higher risk of getting diabetes than average people in the future."));
+        assertTrue(sug.contains("Since your mother have gestational diabetes mellitus, you would bear more threats of getting diabetes."));
+        assertTrue(sug.contains("You blood fat looks high, which may be a signal or complication of diabetes."));
+        assertTrue(!sug.contains("Diabetes is mostly a life-length disease, so keep checking your health condition and monitor blood sugar level."));
+
+        String sum=u.getSuggestionsSummary();
+        assertTrue(sum.equals("We have several helpful information based on your optional description in profile"));
+
+
+        String sugD=u.generateSuggestionD();
+
+        assertTrue(sugD.equals("Based on your profile, It looks you are far from getting diabetes! Though a good lifestyle is important and MEDIUM-GI and LOW-GI food is helpful in the long run."));
+
+
+
+    }
+
+
+
 
 
 
