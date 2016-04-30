c0565598a735b8cd055c79a5d9d89bad882dd501
Qi Zhang <zhang.qi.js@gmail.com>
2016-04-30 17:11:09 -0500

    Refactor and Comments. qizhang4&yjiang32

--- DiabetesTracker/app/src/main/java/com/cs428/dit/diabetestracker/helpers/FoodItemLog.java
+++ DiabetesTracker/app/src/main/java/com/cs428/dit/diabetestracker/helpers/FoodItemLog.java
@@ -2,6 +2,7 @@ package com.cs428.dit.diabetestracker.helpers;
 
 /**
  * Created by QiZhang on 3/9/16.
+ * This class represents how a food item is stored in Firebase.
  */
 public class FoodItemLog {
     String date;
@@ -13,24 +14,40 @@ public class FoodItemLog {
     public FoodItemLog() {
     }
 
+    /**
+     * @param date the date the food item is logged
+     * @param food the Food Object of the food item
+     */
     public FoodItemLog(String date, Food food) {
         this.date = date;
         this.food = food;
 
     }
 
+    /**
+     * @return food Food Object of the food item
+     */
     public Food getFood() {
         return food;
     }
 
+    /**
+     * @param food Food Object of the food item
+     */
     public void setFood(Food food) {
         this.food = food;
     }
 
+    /**
+     * @return date the date the food item is logged
+     */
     public String getDate() {
         return date;
     }
 
+    /**
+     * @param date the date the food item is logged
+     */
     public void setDate(String date) {
         this.date = date;
     }
